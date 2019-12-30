49c9e9bb95e24bf5a15c147051ff328e53a1e752
k-zaha <kzaha@aol.com>
2019-12-30 16:01:19 +0900

    first commit Javaの絵本

---
+++ Dzk.java
@@ -0,0 +1,18 @@
+import java.io.UnsupportedEncodingException;
+
+
+class Dzk{
+    public static void main(String[] args) {
+
+        String a = "Javaの絵本";
+        int len = a.length();
+        String b = a.substring(len-2, len);
+        int c = b.compareTo("絵本");
+
+        System.out.println("「" + a + "」の最後の2文字は「" + b + "」");
+        System.out.println("絵本" + (c==0 ? "です" : "ではありません"));
+
+
+    }
+
+}
