71bc7b8ef2d2665eb2adfd9f9c27a743f8c20999
k-zaha <kzaha@aol.com>
2019-12-31 02:26:00 +0900

    if文を使用した条件分岐

---
+++ Ifbn.java
@@ -0,0 +1,13 @@
+class Ifbn{
+    public static void main(String[] args){
+        int a = 65;
+        if(a < 70){
+            System.out.println("平均点まで、あと" + (70 - a) +"点です。");
+            System.out.println("頑張りましょう！！");        
+        }else{
+            System.out.println("平均点以上の点数です。");
+            System.out.println("すばらしい！");
+
+        }
+    }
+}
