2270658e5dbe0ab128b144b4f5a280a9559bbbaf
k-zaha <kzaha@aol.com>
2019-12-31 00:45:00 +0900

    比較演算子

---
+++ Hikaku.java
@@ -0,0 +1,14 @@
+class Hikaku{
+    public static void main(String[] args){
+/*
+        int a = 2, b = 1;
+        System.out.println((a > b));
+        System.out.println((b > a));
+        System.out.println(a > b);
+        
+        //代入したまま、値を出力できる。
+        int c = 0;
+        System.out.println(c = 8);
+*/
+    }
+}
