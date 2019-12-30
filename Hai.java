546776df6fa1052f51870f084eb250bfcef48999
k-zaha <kzaha@aol.com>
2019-12-30 19:07:27 +0900

    多次元配列　のフィールド作成＆出力

--- Hai.java
+++ Hai.java
@@ -3,7 +3,7 @@ class Hai{
     public static void main(String[] args){
         int[] a = new int[] {1,2,3,4};
         //ArraysクラスのtoStringメソッドを使うことによって[]付きで配列の中身を全出しできる。
-        //System.out.print(Arrays.toString(a));
+        System.out.print(Arrays.toString(a));
 
         //でも、forで繰り返し処理を行って逐次出力(println)にしたほうがシンプル
         for (int i = 0; i < a.length ; i++) {
---
+++ Taji.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Taji{
+    public static void main(String[] args){
+
+        //一次元の配列　変数作成　出力などなど
+/*        int[][] a = {
+            {10, 20},
+            {30, 40},
+            {50, 60}
+        };
+
+        System.out.println("a[0][0]=" + a[0][0] + " a[0][1]=" + a[0][1]);
+        System.out.println("a[1][0]=" + a[0][0] + " a[1][1]=" + a[0][1]);
+        System.out.println("a[2][0]=" + a[0][0] + " a[0][1]=" + a[0][1]);
+*/
+
+        int[][]a = {
+            {10, 20, 30},
+            {40, 50},
+            {60, 70, 80, 90}
+        };
+
+        System.out.println("一次元配列の要素数" + a.length);
+        System.out.println("a[0]の要素数 " + a[0].length);
+        System.out.println("a[1]の要素数 " + a[1].length);
+        System.out.println("a[2]の要素数 " + a[2].length);
+
+    }
+
+}
