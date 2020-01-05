4e945a7eb1456e7055c8a8ab5fb23f732bd6dd4b
k-zaha <kzaha@aol.com>
2020-01-05 14:15:44 +0900

    円形、四角形の面積と外周を求めるプログラム

---
+++ EnumSample.java
@@ -0,0 +1,46 @@
+import java.util.List;
+import java.util.ArrayList;
+enum Animal {Cat, Dog, Rabbit }
+
+/*列挙型　enumで自分でデータ型を定義する。
+　enum Animal{Cat, Dog, Rabbit} enumの後に"Animal"と宣言し、データ型"Animal"を生成。
+  Animal型には、Cat, Dog, Rabbitと3種類の値を入れる事ができる。逆にこの3種類以外は入れられない。
+
+  最初に、データ型の名前と、使用する値を定義しておくのが肝。
+
+  メソッドの引数や変数に、この列挙型を使用すると、プログラムが見やすくなり、間違えにくいプログラムを
+  書くことができる。
+*/
+
+class Pet {
+    private Animal kind;  //Privateだから、このフィールドはPetクラス内からしか呼び出せない。
+    private String name;
+    
+    public Pet(Animal aml, String nm) {
+        kind = aml;
+        name = nm;
+    }
+    public Animal getKind(){
+        return kind;
+    }
+    public String getName(){
+        return name;
+    }
+}
+
+class EnumSample {
+    public static void main(String[] args){
+        List<Pet> lst = new ArrayList<Pet>(3);
+        lst.add(new Pet(Animal.Dog, "Suzukikun"));
+        lst.add(new Pet(Animal.Cat, "Nyaomi"));
+        lst.add(new Pet(Animal.Rabbit, "Tigger"));
+        
+        for(Pet pt : lst) {
+            
+            if(pt.getKind() == Animal.Dog) {
+                System.out.println("Dog name: " + pt.getName());
+            }
+        }
+
+    }
+}
---
+++ SizeFigure.java
@@ -0,0 +1,90 @@
+/*
+    抽象クラスFigureと、それを継承するクラスSquare、Circleを用意。
+
+    Square == 四角形の面積と外周を表す。
+    コンストラクタを利用し、インスタンス生成時に、四角形の横と縦を読み込む仕組みとなっているが、
+    コンストラクタをオーバーロードし、正方形の場合は、引数1個で自動的に縦、横サイズが読み込まれるようになっている。
+
+    Circle == 円の面積と外周を表す。
+    コンストラクタを利用し、インスタンス生成時に円の直径を読み込む仕組みとなっている。
+    円周率3.14は不変なので、Circleクラスでフィールド定義時にfinal修飾子を付けて、定数化。
+
+    
+    実行クラスSizeFigureにて、
+    インスタンス生成の宣言はFigureクラス、データ型の宣言はサブクラスのSquareやCircleを指定しているが、
+    インスタンス生成の宣言をサブクラスで行っても問題ない。
+
+    スーパークラス側でインスタンス生成の宣言を行うと、別の型に変更しようとした際に、元のデータに手を加える必要がない。
+    というメリットがある。
+    しかし、インスタンス生成の宣言をサブクラスで行うと、サブクラス側にしかないメソッドやフィールドを使いやすい
+    メリットがある。
+
+    
+*/
+
+abstract class Figure {
+    abstract void area();  //面積
+    abstract void around(); //外周
+
+    void measure() {
+        area();
+        around();
+        System.out.println();
+    }
+}
+
+// 抽象クラスのサブクラスsquare
+class Square extends Figure {
+    double side;
+    double height;
+
+    Square(double s){  //Squareのコンストラクタ
+        side = height = s;
+    }
+
+    Square(double s, double h){ //コンストラクタのオーバーロード
+        side = s;
+        height = h;
+    }
+
+    void area() { //抽象メソッドのオーバーライド
+        System.out.println("四角形の面積：" + (side * height));
+    }
+
+    void around() { //抽象メソッドのオーバーライド
+        System.out.println("四角形の外周：" + (2 * (side * height)));
+    }
+}
+
+class Circle extends Figure {
+    final double pi = 3.14;
+    double radius;
+
+    Circle(double r) {
+        radius = r;
+    }
+
+    void area() { //抽象メソッドのオーバーライド
+        System.out.println("円の面積：" + (radius * radius * pi));
+    }
+
+    void around() { //抽象メソッドのオーバーライド
+        System.out.println("円の外周：" + (2 * radius * pi));
+    }
+}
+
+
+class SizeFigure {
+    public static void main(String[] args) {
+        Figure fig1 = new Square(2.5);
+        Figure fig2 = new Square(2.3, 3.7);
+        Circle fig3 = new Circle(3.6);
+        //Figure fig4 = new Circle(5);
+        fig1.measure();
+        fig2.measure();
+        fig3.measure();
+        //fig4.measure();
+    
+
+    }
+}
